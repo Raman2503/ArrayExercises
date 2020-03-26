2552b86700c7bb1cc2c6dcf9f10ec5414b37e5fa
Raman Khosravi <ramankhosravi@yahoo.de>
2020-03-26 11:06:18 +0100

    Started exercise for 2D array.

---
+++ ArrayExercises/Logic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayExercises
+{
+	public class Logic
+	{
+		public void FillUpperRightWithZeros()
+		{}
+
+		public void FillUpperLeftWithZeros()
+		{ }
+
+		public void FillLowerRightWithZeros()
+		{ }
+
+		public void FillLowerLeftWithZeros()
+		{ }
+
+	}
+}
--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,32 @@ namespace ArrayExercises
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int rows = 0;
+			int columns = 0;
+
+			int[,] array = new int[rows, columns];
+
+
+			Console.WriteLine("Insert number of rows M");
+
+			rows = Convert.ToInt32(Console.ReadLine());
+			
+			Console.WriteLine("Insert number of columns N");
+
+			columns = Convert.ToInt32(Console.ReadLine());
+					   
+			Console.WriteLine("Insert numbers");
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < columns; j++)
+				{
+					array[i, j] = int.Parse(Console.ReadLine());
+					Console.WriteLine(array[i,j]);
+					Console.ReadLine();
+				}
+			}
+
 		}
 	}
 }
