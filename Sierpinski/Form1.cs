ff12c5d1ae3722eff0364196892a851bea4c2a6d
Raman Khosravi <ramankhosravi@yahoo.de>
2020-04-03 16:14:58 +0200

    Edited function DrawSerpinskiTriangle.

--- Sierpinski/Form1.cs
+++ Sierpinski/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Sierpinski
@@ -15,14 +9,12 @@ namespace Sierpinski
 		private Point p1;
 		private Point p2;
 		private Point p3;
-		int depth = 4;
+		int depth = 3;
 
 		public Form1()
 		{
 			InitializeComponent();
 		}
-		
-	
 
 		private void Draw_Button_Click_1(object sender, EventArgs e)
 		{
@@ -31,17 +23,9 @@ namespace Sierpinski
 			p2 = new Point(50, 300);
 			p3 = new Point(550, 300);
 
-			// Function call
-			Logic.DrawSerpinskiTriangle(p1, p2, p3, depth);
-			
-			// Draw in UI
 			var picture = pictureBox.CreateGraphics();
-			picture.DrawPolygon(new Pen(Color.Black), new Point[]
-				{
-					p1,
-					p2,
-					p3,
-				});
+			// Function call
+			Logic.DrawSerpinskiTriangle(p1, p2, p3, depth, picture);
 		}
 	}
 }
--- Sierpinski/Logic.cs
+++ Sierpinski/Logic.cs
@@ -1,32 +1,39 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Sierpinski
 {
 	public static class Logic
 	{
 		private static int counter;
-
-		public static void DrawSerpinskiTriangle(Point p1, Point p2, Point p3, int depth)
+		public static void DrawSerpinskiTriangle(Point p1, Point p2, Point p3, int depth, Graphics pictureBox)
 		{
-			// end condition
-			counter++;
-			if (counter == depth)
+			// end condition			
+			if(depth == counter)
 			{
-				return;
-			}
 
-			else
-			{
+			}
 				// calculate middle points of each side			
 				Point middlePoint12 = new Point((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2);
 				Point middlePoint13 = new Point((p1.X + p3.X) / 2, (p1.Y + p3.Y) / 2);
 				Point middlePoint23 = new Point((p2.X + p3.X) / 2, (p2.Y + p3.Y) / 2);
 
-				// recursive call
-				DrawSerpinskiTriangle(p1, middlePoint12, middlePoint13, depth);
-				DrawSerpinskiTriangle(middlePoint12, p2, middlePoint23, depth);
-				DrawSerpinskiTriangle(middlePoint13, middlePoint23, p3, depth);
+				// recursive call for first time
+				DrawSerpinskiTriangle(p1, middlePoint12, middlePoint13, depth, pictureBox);
+				DrawSerpinskiTriangle(middlePoint12, p2, middlePoint23, depth, pictureBox);
+				DrawSerpinskiTriangle(middlePoint13, middlePoint23, p3, depth, pictureBox);
+
+				counter++;
+
+				pictureBox.DrawPolygon(new Pen(Color.Black), new Point[]
+				{
+					p1,
+					p2,
+					p3,
+					});
+				}
 			}
-		}
 	}
-}
+
+
