bbd51eca79ac8028c22f38955857567cf8fa54cd
Raman Khosravi <raman.khosravi@txtgroup.com>
2020-05-24 19:43:12 +0200

    Created Person class with name and age property.

---
+++ SortArray/Person.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace SortArray
+{
+	 internal class Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+
+		public Person(string name, int age)
+		{
+			Name = name;
+			Age = age;
+		}
+
+		List<Person> persons = new List<Person>()
+		{
+			new Person("Bob", 79),
+			new Person("Paul", 26),
+			new Person("Steve", 31),
+			new Person("Cory", 54),
+			new	Person("Sara", 36),
+			new Person("Joe",70)
+		}; 
+	}
+}
