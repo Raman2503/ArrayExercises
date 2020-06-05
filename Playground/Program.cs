175833db42a9c859f177792ed0d81e5a700f1159
Raman Khosravi <raman.khosravi@txtgroup.com>
2020-06-05 19:47:11 +0200

    Added DI container demo.

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Autofac;
 
 namespace Playground
 {
@@ -21,6 +22,11 @@ namespace Playground
 
 		static void Main(string[] args)
 		{
+			//
+
+			DIContainerDemo();
+			//
+
 
 			foreach (var item in GetSomeNumbers())
 			{
@@ -69,7 +75,6 @@ namespace Playground
 			}
 
 			return;
-			#region Foreach internals
 
 			{
 				List<int> list = new List<int>() { 1, 2, 3, 4 };
@@ -94,13 +99,140 @@ namespace Playground
 					enumerator.Dispose();
 				}
 			}
-
-			#endregion
-
-
-
-
+					   			 		  
 			Console.ReadLine();
+		}
+
+
+
+
+		class A {}
+
+		class B
+		{
+			private readonly A _pA;
+			private readonly IConfig _pConfig;
+
+			public B(A pA)
+			{
+				_pA = pA;
+				//_pConfig = pConfig;
+			}
+		}
+
+		class C
+		{
+			private readonly A _pA;
+			private readonly B _pB;
+			private readonly CalculatorFactory _pCalcFactory;
+			private readonly IConfig _pConfig;
+			private readonly ILogger _pLogger;
+
+			public C(A pA, B pB, CalculatorFactory pCalcFactory)
+			{
+				_pA = pA;
+				_pB = pB;
+				_pCalcFactory = pCalcFactory;
+
+				var calculator = _pCalcFactory.Create();
+			}
+		}
+
+		interface ICalculator {
+			void Calculate();
+		}
+
+		interface IConfig
+		{
+			void Configure();
+		}
+
+		private class Config : IConfig
+		{
+			public void Configure()
+			{
+
+			}
+		}
+
+		public class Calc : ICalculator
+		{
+			private readonly ILogger _logger;
+
+			public Calc(ILogger logger)
+			{
+				_logger = logger;
+			}
+
+			public void Calculate()
+			{
+				//_logger
+			}
+		}
+		public interface ILogger { }
+
+		private class NullLogger : ILogger
+		{
+		}
+
+		public class Logger : ILogger
+		{
+			
+		}
+
+		class CalculatorFactory
+		{
+			private readonly ILogger _logger;
+
+			public CalculatorFactory(ILogger logger)
+			{
+				_logger = logger;
+			}
+			public ICalculator Create()
+			{
+				return new Calc(_logger);
+			}
+		}
+
+		private static void DIContainerDemo()
+		{
+			//var pA = new A();
+			
+			//ILogger logger = new Logger();
+			//IConfig config = new Config();
+
+			//var calculatorFactory = new CalculatorFactory(logger);
+			//var c = new C(pA, new B(pA), calculatorFactory, config);
+			//var c2 = new C(pA, new B(pA), calculatorFactory, config);
+			//var c3 = new C(pA, new B(pA), calculatorFactory, config);
+			//var c4 = new C(pA, new B(pA), calculatorFactory, config);
+
+			// Using container
+			var builder = new ContainerBuilder();
+
+			// Composition root (UI solution)
+			builder.RegisterType<Logger>()
+				.As<ILogger>()
+				.SingleInstance();
+
+			builder.RegisterType<C>();
+			builder.RegisterType<A>();
+			builder.RegisterType<B>();
+			builder.RegisterType<CalculatorFactory>();
+			builder.RegisterType<Config>().As<IConfig>();
+
+			builder.RegisterType<NullLogger>().As<ILogger>();
+
+			var container = builder.Build();
+
+			var logger2 = container.Resolve<ILogger>();
+			var c5 = container.Resolve<C>();
+
+			// override some registrations for testing
+
+			var c6 = container.Resolve<C>();
+
+
 		}
 	}
 }
